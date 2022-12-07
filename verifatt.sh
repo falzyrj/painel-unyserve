df03ba9ac9526abdce27f866a20654750919a495
falzyrj <99560238+falzyrj@users.noreply.github.com>
2022-12-07 19:22:15 -0300

    Initial commit

---
+++ verifatt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+link1=bitbucket.org/nandoslayer/4g/downloads/verpw4g
+link2=bitbucket.org/nandoslayer/4g/downloads/verweb
+[[ -e /home/verpw4g ]] && rm /home/verpw4g
+wget -P /home $link1 > /dev/null 2>&1
+[[ -f "/home/verpw4g" ]] && {
+vrs1=$(sed -n '1 p' /etc/kernel/recweb/verpw4g| sed -e 's/[^0-9]//ig')
+vrs2=$(sed -n '1 p' /home/verpw4g | sed -e 's/[^0-9]//ig')
+[[ "$vrs1" != "$vrs2" ]] && mv /home/verpw4g /etc/kernel/recweb/attpw4g
+}
+[[ -e /home/verweb ]] && rm /home/verweb
+wget -P /home $link2 > /dev/null 2>&1
+[[ -f "/home/verweb" ]] && {
+vrs3=$(sed -n '1 p' /etc/kernel/recweb/verweb| sed -e 's/[^0-9]//ig')
+vrs4=$(sed -n '1 p' /home/verweb | sed -e 's/[^0-9]//ig')
+[[ "$vrs3" != "$vrs4" ]] && mv /home/verweb /etc/kernel/recweb/attweb
+}
