e320eb25905adfe5136e5196fa78c6456b4a5318
falzyrj <99560238+falzyrj@users.noreply.github.com>
2022-12-12 09:43:00 -0300

    up

--- internet/cronphp/cron.autobackup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-[[ ! -d /root/backupsql ]] && mkdir /root/backupsql
-rm /root/backupsql/net.sql > /dev/null 2>&1
-senha=$(grep -E "3" /var/www/html/conexao.php| cut -d"'" -f2)
-mysqldump -u root -p$senha net > /root/backupsql/net.sql
-bzip2 -f /root/backupsql/net.sql
